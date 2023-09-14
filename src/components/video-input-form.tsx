d684ce2cb7d2eda7339348685346667dbb2bd1c8
Tiago Vieira <tiagosvieira10@gmail.com>
2023-09-14 10:54:14 -0300

    criacao de components

---
+++ src/components/video-input-form.tsx
@@ -0,0 +1,50 @@
+import { FileVideo, Upload } from "lucide-react";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
+import { Label } from "./ui/label";
+import { Separator } from "./ui/separator";
+import { ChangeEvent, useState } from "react";
+
+export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget
+
+    if (!files) {
+      return
+    }
+
+    const selectedFile = files[0]
+  }
+
+  return (
+    <form className="space-y-6">
+    <label 
+      htmlFor="video"
+      className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+    >
+      <FileVideo className="w-4 h-4" />
+      Selecione um vídeo
+    </label>
+    
+    <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected}/>
+
+    <Separator/>
+
+    <div className="space-y-2">
+      <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+      <Textarea 
+        id="transcription_prompt" 
+        className="h-20 leading-relaxed resize-none"
+        placeholder="Inclua palavras-chaves mencionadas no vídeo separadas por vírgula (,)"
+      />
+    </div>
+
+    <Button type="submit" className="w-full">
+      Carregar vídeo
+      <Upload className="w-4 h-4 ml-2"/>
+    </Button>
+  </form>
+  )
+}
